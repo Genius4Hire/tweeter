3756ab76a2a905f2960df921fe7f0b15372293a9
Jordan Dennis <jordan.dennis@gmail.com>
2023-06-23 04:30:05 +0000

    Added a tweet template to list old tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,31 +1,51 @@
 $(document).ready(function() {
 
 
+  $(".article-div").hover(function() {
+      $(this).css("box-shadow", "5px 5px 15px 5px rgba(0,0,0,0.45)")
+    }, 
+    function() {
+      $(this).css("box-shadow", "5px 5px 15px 5px rgba(0,0,0,0)")
+    }
+  );
+
+
+  $(".flag-icon").hover(function() {
+    $(this).css("color", "#cb9826")
+  }, 
+  function() {
+    $(this).css("color", "#49569c")
+  });
+
+  $(".retweet-icon").hover(function() {
+    $(this).css("color", "#cb9826")
+  }, 
+  function() {
+    $(this).css("color", "#49569c")
+  });
+
+  $(".like-icon").hover(function() {
+    $(this).css("color", "#cb9826")
+  }, 
+  function() {
+    $(this).css("color", "#49569c")
+  });
+
 
   $("#new-tweet-textarea").keyup(function() {
     const maxLength = 140;
     const currentText = $(this).val();
     const currentLength = $(this).val().length;
     const charsRemaining = maxLength - currentLength;
-
+    $("#new-tweet-counter").text(charsRemaining)
     if (charsRemaining < 0) {
       $("#new-tweet-counter").css({'color': '#ff0000'});
     } else {
       $("#new-tweet-counter").css({'color': '#555149'});
-
     };
 
-    $("#new-tweet-counter").text(charsRemaining);
 
   });
-  
 
-  // put a reference to the "div-two" element in a variable
-  // const textArea = document.getElementById("new-tweet-textarea");
-  // const tweetLength = document.getElementById("new-tweet-counter")
-  
-  // // when div2 is clicked, run the function
-  // textArea.addEventListener('keydown', );
-  // //tweetLength.addEventListener('')
 
 });
