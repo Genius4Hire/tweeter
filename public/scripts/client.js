06b8d495b0e076e56b994cb467d2ed06a4abad61
Jordan Dennis <jordan.dennis@gmail.com>
2023-06-25 02:52:47 +0000

    Added AJAX functions to GET and POST to localhost:8080/tweets.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,34 +1,8 @@
 $(() => {
-  
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  
-  function createTweetElement(tweet) {
-    const englishDate = new Date(tweet.created_at).toLocaleDateString("en-US");
-    // prevent cross site    
-    let html = `
+  const createTweetElement = function(tweet) {
+    const timeStampUNIX = new Date(tweet.created_at).toLocaleDateString("en-US");
+    const timeAgo = timeago.format(timeStampUNIX);
+    let tweetHTML = `
     <section class="past-tweet">
       <article class="tweet">
         <header>
@@ -42,7 +16,7 @@ $(() => {
           <a class="tweet-content">${tweet.content.text}</a>
         </div>
         <footer>
-          <a>${englishDate}</a>
+          <a>${timeAgo}</a>
           <a>
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -53,51 +27,54 @@ $(() => {
     </section>
     `;
    // let tweetElement = $tweet.append(html)
-    return html;
+    $('.container').prepend(tweetHTML); 
   };
 
-  function renderTweets (tweets) {
-    let tweetsContainer = $('.container');
+  const renderTweets = function(tweets) {
+    const tweetsContainer = $('.container');
+    // tweetsContainer.empty();
+
+
     for (tweet of tweets) {
-      let tweetElement = createTweetElement(tweet); 
-      tweetsContainer.prepend(tweetElement); 
+      createTweetElement(tweet); 
     }
 
   }
   
-  const $newTweetForm = $('.new-tweet form'); //grab the form
+  const loadTweets = function() {
+    // make a request to /tweets and receive the array of tweets
+    // receive the array of tweets as JSON
+    // call it right after its definition.
+    // our success callback function will simply call up renderTweets,passing it the response from the AJAX
+    $.ajax({
+      url: 'http://localhost:8080/tweets',
+      method: 'GET',
+      success: (tweetDB) => {
+        renderTweets(tweetDB);
+      },
+      fail: (err) => {
+        console.log(err);
+      }
+    });
+  };
+
+  loadTweets();
+
+  const $newTweetForm = $('.new-tweet form'); //grab the form as a jquery object;
 
   $newTweetForm.on('submit', function(event) {
-    //event.preventDefault();
-    const tweetContents = $('textarea#tweet-text.tweet-content').val();
-    const tweetLength = tweetContents.length;
-    console.log(tweetContents);
-    console.log(tweetLength);
-    // // over limit?
-    // if (tweetLength > 140) {
-    //   return;
-    // }
-
-    // // no content?
-    // if (!tweetLength) {
-    //   return;
-    // }
-    // // serialize the form data and send it to the server as a query string
-    // const data = $newTweetForm.serialize();
-
-    const data = $newTweetForm.serialize();
-    console.log($newTweetForm.serialize());
+    event.preventDefault();
+    let tweetContents = $('textarea#tweet-text.tweet-content').val();
+    let URLParams = $newTweetForm.serialize();
 
     $.ajax({
       url: 'http://localhost:8080/tweets',
       method: 'POST',
-      data: data,
+      data: URLParams,
       success: () => {
-        $('.exceeds-limit').hide();
-        $('.no-content').hide();
+        loadTweets();
       }
     });
   });
 
-   renderTweets(data);
 });
